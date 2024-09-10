42bed5fca0fdad500fb502d41ccfaf38f6ae6957
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-10 18:38:05 +0300

    Create tests for /api/tender/new

--- mysrc/tests/test_db.py
+++ mysrc/tests/test_db.py
@@ -31,19 +31,52 @@ async def test_create_tender_good(client: TestClient, async_session: AsyncSessio
     assert response_data["serviceType"] == data["serviceType"]
     assert response_data["version"] == 1
 
-# async def test_create_tender_bad(client: TestClient, async_session: AsyncSession):
-#     new_organization = await create_test_organization(async_session)
-#     new_employee = await create_test_employee(async_session)
-#
-#     data = {
-#         "name": "New Tender",
-#         "description": "This is a test tender",
-#         "serviceType": TenderServiceType.construction,
-#         "status": TenderStatus.created,
-#         "organizationId": new_organization.id,
-#         "creatorUsername": 'not_existing_username'
-#     }
-#
-#     response = client.post("/api/tender/new", json=data)
-#     assert response.status_code == 200
-#     res = response.json()
+
+async def test_create_tender_bad_creator(client: TestClient, async_session: AsyncSession):
+    new_organization = await create_test_organization(async_session)
+    new_employee = await create_test_employee(async_session)
+
+    data = {
+        "name": "New Tender",
+        "description": "This is a test tender",
+        "serviceType": TenderServiceType.construction,
+        "status": TenderStatus.created,
+        "organizationId": new_organization.id,
+        "creatorUsername": 'not_existing_username'
+    }
+
+    response = client.post("/api/tender/new", json=data)
+    assert response.status_code == 404
+
+
+async def test_create_tender_bad_organisation(client: TestClient, async_session: AsyncSession):
+    new_organization = await create_test_organization(async_session)
+    new_employee = await create_test_employee(async_session)
+
+    data = {
+        "name": "New Tender",
+        "description": "This is a test tender",
+        "serviceType": TenderServiceType.construction,
+        "status": TenderStatus.created,
+        "organizationId": 999,
+        "creatorUsername": new_employee.username
+    }
+
+    response = client.post("/api/tender/new", json=data)
+    assert response.status_code == 404
+
+
+async def test_create_tender_not_enough_data(client: TestClient, async_session: AsyncSession):
+    new_organization = await create_test_organization(async_session)
+    new_employee = await create_test_employee(async_session)
+
+    data = {
+        "name": "New Tender",
+        "serviceType": TenderServiceType.construction,
+        "status": TenderStatus.created,
+        "organizationId": new_organization.id,
+        "creatorUsername": new_employee.username
+    }
+
+    response = client.post("/api/tender/new", json=data)
+    assert response.status_code == 422
