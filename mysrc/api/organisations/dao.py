309e4880cbd9705d5edfa43cf23d3476c07e7cea
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-11 18:28:27 +0300

    Add check_is_user_responsible

--- mysrc/api/organisations/dao.py
+++ mysrc/api/organisations/dao.py
@@ -2,11 +2,12 @@ from fastapi import HTTPException
 from sqlalchemy import select
 
 from mysrc.api.dao import DAO
-from mysrc.api.organisations.models import MOrganization
+from mysrc.api.organisations.models import MOrganization, MOrganizationResponsible
 
 
 class OrganizationCRUD(DAO):
     crud_model = MOrganization
+
     async def _get_organisation_by_id(self, organization_id: int) -> MOrganization:
         m_organization = await self.db.execute(select(MOrganization).where(MOrganization.id == organization_id))
         m_organization = m_organization.scalar_one_or_none()
@@ -14,4 +15,12 @@ class OrganizationCRUD(DAO):
             raise HTTPException(status_code=404, detail=f"Organization with id={organization_id} not found.")
         return m_organization
 
-
+    async def check_is_user_responsible(self, organization_id: int, user_id: int) -> bool:
+        query = (
+            select(MOrganizationResponsible).
+            where(MOrganizationResponsible.organization_id == organization_id).
+            where(MOrganizationResponsible.user_id == user_id)
+        )
+        if not (await self.db.execute(query)).scalar_one_or_none():
+            return False
+        return True
