d0e5d9c36be62718296875bfcd9e2e3988fb0491
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-11 15:02:47 +0300

    Create binds DAO and API

---
+++ mysrc/api/binds/dao.py
@@ -0,0 +1,191 @@
+from fastapi import HTTPException
+from sqlalchemy import select, desc
+
+from mysrc.api.binds.models import MBind, MBindData, BindStatus
+from mysrc.api.binds.schemas import SBindCreate, SBindRead, SBindUpdate
+from mysrc.api.dao import DAO
+from mysrc.api.employees.dao import EmployeeCRUD
+from mysrc.api.organisations.dao import OrganizationCRUD
+
+
+class BindCRUD(DAO):
+    async def _add_bind_to_bind_db(
+            self, status: BindStatus, tender_id, organization_id: int, creator_id: int
+    ) -> MBind:
+        m_bind = MBind(
+            status=status,
+            tender_id=tender_id,
+            organization_id=organization_id,
+            creator_id=creator_id,
+        )
+        self.db.add(m_bind)
+        await self.db.commit()
+        await self.db.refresh(m_bind)
+        return m_bind
+
+    async def _add_bind_to_bind_data_db(
+            self, bind_id: int, name: str, description: str, version: int, **kwargs
+    ) -> MBindData:
+        m_bind_data = MBindData(
+            bind_id=bind_id,
+            name=name,
+            description=description,
+            version=version,
+        )
+        self.db.add(m_bind_data)
+        await self.db.commit()
+        await self.db.refresh(m_bind_data)
+        return m_bind_data
+
+    async def _get_bind_by_id(self, bind_id: int) -> MBind:
+        query = select(MBind).where(MBind.id == bind_id)
+        m_bind = await self.db.execute(query)
+        m_bind = m_bind.scalar_one_or_none()
+        if not m_bind:
+            raise HTTPException(status_code=404, detail=f"Bind with id={bind_id} not found")
+        return m_bind
+
+    async def _get_bind_data_with_last_version_by_id(self, bind_id: int) -> MBindData:
+        query = (
+            select(MBindData).
+            where(MBindData.bind_id == bind_id).
+            order_by(desc(MBindData.version)).
+            limit(1)
+        )
+        m_bind_data_with_last_version = await self.db.execute(query)
+        m_bind_data_with_last_version = m_bind_data_with_last_version.scalar_one_or_none()
+        if not m_bind_data_with_last_version:
+            raise HTTPException(status_code=404, detail=f"Bind data for bind with id={bind_id} not found.")
+        return m_bind_data_with_last_version
+
+    async def _get_bind_data_by_version(self, bind_id: int, version: int) -> MBindData:
+        query = (
+            select(MBindData).
+            where(MBindData.bind_id == bind_id).
+            where(MBindData.version == version)
+        )
+        m_bind_data = await self.db.execute(query)
+        m_bind_data = m_bind_data.scalar_one_or_none()
+        if not m_bind_data:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Bind data with version={version} for bind with id={bind_id} not found."
+            )
+        return m_bind_data
+
+    async def get_response_schema(
+            self,
+            bind_id: int | None = None,
+            bind: MBind | None = None,
+            bind_data: MBindData | None = None,
+    ) -> SBindRead:
+        if not bind_id:
+            if bind:
+                bind_id = bind.id
+            elif bind_data:
+                bind_id = bind_data.bind_id
+            else:
+                raise HTTPException(status_code=500, detail="One of bind_id, bind, bind_data must be provided")
+
+        if not bind:
+            bind: MBind = await self._get_bind_by_id(bind_id)
+        if not bind_data:
+            bind_data: MBindData = await self._get_bind_data_with_last_version_by_id(bind_id)
+        return SBindRead(
+            id=bind.id,
+            name=bind_data.name,
+            description=bind_data.description,
+            status=bind.status,
+            version=bind_data.version,
+            createdAt=bind.created_at,
+        )
+
+
+class BindDAO(BindCRUD, OrganizationCRUD, EmployeeCRUD):
+    async def create_bind(self, bind: SBindCreate) -> SBindRead:
+        # проверка существования организации с id=bind.organizationId
+        await self._get_organisation_by_id(organization_id=bind.organizationId)
+
+        # проверка существования пользователя с username=bind.creatorUsername
+        creator = await self._get_employee_by_username(username=bind.creatorUsername)
+
+        # добавление тендера в БД тендеров
+        m_bind = await self._add_bind_to_bind_db(
+            status=bind.status,
+            tender_id=bind.tenderId,
+            organization_id=bind.organizationId,
+            creator_id=creator.id,
+        )
+        # добавление тендера в БД версий тендеров
+        m_bind_data = await self._add_bind_to_bind_data_db(
+            bind_id=m_bind.id,
+            name=bind.name,
+            description=bind.description,
+            service_type=bind.serviceType,
+            version=1,
+        )
+        return await self.get_response_schema(bind=m_bind, bind_data=m_bind_data)
+
+    async def update_bind_by_id(self, bind_id: int, bind_update_data: SBindUpdate, username: str):
+        # проверка прав доступа
+        # await self._check_auth(username=username)
+
+        # получение данных последней версии по предложению
+        m_bind_data_with_last_version = await self._get_bind_data_with_last_version_by_id(bind_id)
+
+        # обновление данных предложения
+        new_bind_data = {**m_bind_data_with_last_version.__dict__}
+        for key, value in bind_update_data.model_dump(exclude_unset=True).items():
+            new_bind_data[key] = value
+        new_bind_data['version'] += 1
+
+        # добавление новых данных в БД данных предложений
+        m_new_bind_data = await self._add_bind_to_bind_data_db(**new_bind_data)
+
+        return await self.get_response_schema(bind_id=bind_id, bind_data=m_new_bind_data)
+
+    async def get_binds_by_kwargs(self, **kwargs):
+        limit: int | None = kwargs.get('limit', None)
+        offset: int | None = kwargs.get('offset', None)
+        tender_id: int | None = kwargs.get('tender_id', None)
+        username: str | None = kwargs.get('username', None)
+
+        query = select(MBind)
+        if username:
+            creator = await self._get_employee_by_username(username=username)
+            query = query.filter(MBind.creator_id == creator.id)
+        m_binds = await self.db.execute(query)
+        m_binds = m_binds.scalars()
+
+        bind_schemas = []
+        for m_bind in m_binds:
+            if tender_id and m_bind.tender_id != tender_id:
+                continue
+            m_bind_data = await self._get_bind_data_with_last_version_by_id(m_bind.id)
+            bind_schemas.append(await self.get_response_schema(bind=m_bind, bind_data=m_bind_data))
+
+        bind_schemas.sort(key=lambda x: x.name)
+        return bind_schemas[offset:offset + limit]
+
+    async def get_bind_status_by_id(self, bind_id: int, username: str):
+        m_bind = await self._get_bind_by_id(bind_id)
+        return m_bind.status
+
+    async def change_bind_status_by_id(self, bind_id: int, status: BindStatus, username: str):
+        m_bind = await self._get_bind_by_id(bind_id)
+        setattr(m_bind, 'status', status)
+        await self.db.commit()
+        return m_bind
+
+    async def rollback_bind(self, bind_id: int, version: int, username: str):
+        m_bind_data_with_given_version = await self._get_bind_data_by_version(bind_id, version)
+        m_bind_data_with_last_version = await self._get_bind_data_with_last_version_by_id(bind_id)
+        m_new_bind_data = MBindData(
+            bind_id=bind_id,
+            version=m_bind_data_with_last_version.version + 1,
+            name=m_bind_data_with_given_version.name,
+            description=m_bind_data_with_given_version.description,
+        )
+        self.db.add(m_new_bind_data)
+        await self.db.commit()
+        return m_new_bind_data
--- mysrc/api/binds/router.py
+++ mysrc/api/binds/router.py
@@ -1,85 +1,87 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from mysrc.api.binds.dao import BindDAO
+from mysrc.api.binds.models import BindStatus
+from mysrc.api.binds.schemas import SBindCreate, SBindUpdate
 
 router = APIRouter(
     prefix="/api",
     tags=["Binds"],
 )
 
-@router.get("/binds/")
-async def binds():
-    pass
 
-# @router.post("/tender/new")
-# async def create_tender(
-#         tender: STenderCreate,
-#         dao: TenderDAO = Depends()
-# ):
-#     return await dao.create_tender(tender)
-#
-#
-# @router.get("/tenders/")
-# async def get_all_tenders_by_filter(
-#         limit: int = 5,
-#         offset: int = 0,
-#         service_type: Optional[TenderServiceType] = None,
-#         dao: TenderDAO = Depends()
-# ):
-#     return await dao.get_tenders_by_kwargs(
-#         limit=limit,
-#         offset=offset,
-#         service_type=service_type
-#     )
-#
-#
-# @router.get("/tenders/my")
-# async def get_tenders_by_user(
-#         username: str,
-#         limit: int = 5,
-#         offset: int = 0,
-#         dao: TenderDAO = Depends()
-# ):
-#     return await dao.get_tenders_by_kwargs(
-#         limit=limit,
-#         offset=offset,
-#         username=username
-#     )
-#
-#
-# @router.get("/tenders/{tenderId}/status")
-# async def get_tender_status_by_id(
-#         tenderId: int,
-#         username: str,
-#         dao: TenderDAO = Depends()
-# ):
-#     return await dao.get_tender_status_by_id(tenderId, username)
-#
-#
-# @router.put("/tenders/{tenderId}/status")
-# async def change_tender_status_by_id(
-#         tenderId: int,
-#         status: TenderStatus,
-#         username: str,
-#         dao: TenderDAO = Depends()
-# ):
-#     return await dao.change_tender_status_by_id(tenderId, status, username)
-#
-#
-# @router.patch("/tenders/{tenderId}/edit")
-# async def change_tender_status_by_id(
-#         tenderId: int,
-#         tender_update_data: STenderUpdate,
-#         username: str,
-#         dao: TenderDAO = Depends()
-# ):
-#     return await dao.update_tender_by_id(tenderId, tender_update_data, username)
-#
-#
-# @router.put("/tenders/{tenderId}/rollback/{version}")
-# async def change_tender_status_by_id(
-#         tenderId: int,
-#         version: int,
-#         username: str,
-#         dao: TenderDAO = Depends()
-# ):
-#     return await dao.rollback_tender(tenderId, version, username)
-#
+@router.post("/binds/new")
+async def create_bind(
+        tender: SBindCreate,
+        dao: BindDAO = Depends()
+):
+    return await dao.create_bind(tender)
+
+
+@router.get("/binds/my")
+async def get_tenders_by_user(
+        username: str,
+        limit: int = 5,
+        offset: int = 0,
+        dao: BindDAO = Depends()
+):
+    return await dao.get_binds_by_kwargs(
+        limit=limit,
+        offset=offset,
+        username=username
+    )
+
+
+@router.get("/binds/{tenderId}/list")
+async def get_tenders_by_user(
+        username: str,
+        tenderId: int,
+        limit: int = 5,
+        offset: int = 0,
+        dao: BindDAO = Depends()
+):
+    return await dao.get_binds_by_kwargs(
+        limit=limit,
+        offset=offset,
+        username=username,
+        tender_id=tenderId
+    )
+
+
+@router.get("/binds/{bindId}/status")
+async def get_bind_status_by_id(
+        bindId: int,
+        username: str,
+        dao: BindDAO = Depends()
+):
+    return await dao.get_bind_status_by_id(bindId, username)
+
+
+@router.put("/binds/{bindId}/status")
+async def change_bind_status_by_id(
+        bindId: int,
+        status: BindStatus,
+        username: str,
+        dao: BindDAO = Depends()
+):
+    return await dao.change_bind_status_by_id(bindId, status, username)
+
+
+@router.patch("/binds/{bindId}/edit")
+async def edit_bind(
+        bindId: int,
+        bind_update_data: SBindUpdate,
+        username: str,
+        dao: BindDAO = Depends()
+):
+    return await dao.update_bind_by_id(bindId, bind_update_data, username)
+
+
+@router.put("/binds/{bindId}/rollback/{version}")
+async def rollback_bind(
+        bindId: int,
+        version: int,
+        username: str,
+        dao: BindDAO = Depends()
+):
+    return await dao.rollback_bind(bindId, version, username)
