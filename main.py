48156423d6384fd4b5257fb6fa7116a5eae834ce
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-12 16:00:55 +0300

    Add test_postgres

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import RedirectResponse
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.tenders.router import router as tenders_router
 from src.api.binds.router import router as binds_router
-from src.database import init_db
+from src.database import init_db, get_db
 from src.api.tenders.models import MTender, MTenderData
 from src.api.binds.models import MBind, MBindData
 from src.api.organisations.models import MOrganization, MOrganizationResponsible
@@ -18,6 +20,12 @@ app.include_router(binds_router)
 @app.get("/")
 def root():
     return RedirectResponse('/docs')
+
+@app.get("/test_postgres")
+async def test_postgres(db: AsyncSession = Depends(get_db)):
+    quety = select(MEmployee)
+    res = await db.execute(quety)
+    return res.scalars().all()
 
 
 if __name__ == '__main__':
